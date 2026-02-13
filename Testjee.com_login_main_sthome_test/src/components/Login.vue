1e6e606e0f982ee3020452548fcb33f6ab063653
not-vignesh-right <vignesh.bs06@gmail.com>
2026-02-13 20:30:59 +0530

    Optimize logo_test_jee.png from 5.4MB to 133KB and use it in Login

--- Testjee.com_login_main_sthome_test/src/components/Login.vue
+++ Testjee.com_login_main_sthome_test/src/components/Login.vue
@@ -236,7 +236,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '../stores/authStore'
-import logo from '/logo.png'
+import logo from '../assets/logo_test_jee.png'
 
 const router = useRouter()
 const authStore = useAuthStore()
