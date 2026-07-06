f5c01e88b683ac286e00adc75abc2c85d251db15
Chinmay <chinmaypanghri@gmail.com>
2026-07-06 18:38:53 +0530

    Enhance credential hijacking warning text and rejection error message

--- Testjee.com_login_main_sthome_test/src/components/live-exam/ExamLogin.vue
+++ Testjee.com_login_main_sthome_test/src/components/live-exam/ExamLogin.vue
@@ -128,7 +128,7 @@
                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
                  Verification Required
                </h3>
-               <p class="text-xs text-blue-700">This student credential is already claimed. Enter the Roll Number to verify your identity:</p>
+               <p class="text-xs text-blue-700">This student ({{ examStore.sessionDetails.studentName }}) has already entered the lobby or is active in the exam. If this is you, enter your Roll Number to verify your identity. Otherwise, please enter your correct assigned username.</p>
             </div>
 
             <div>
@@ -354,7 +354,7 @@ const submitVerification = () => {
     verificationRollNumber.value = ''
     redirectBasedOnStatus()
   } else {
-    errorMsg.value = 'Incorrect Roll Number for this claimed username. Please verify your assigned username!'
+    errorMsg.value = 'Username already taken and active in the lobby/exam. Enter your correct username as assigned by your teacher!'
   }
 }
 
